e02ef75873869f16f3c4545228fab2915c3bec11
santycano <santy.cano@hotmail.com>
2021-11-05 23:02:43 -0500

    Heroku 5

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/PregnancyWeightGain'));
 ngApp.get('/*', function (request, response) {
-  response.sendFile(path.join(__dirname, '/dist/PregnancyWeightGain/index.html'));
+  response.sendFile(path.join(__dirname, '/src/app/app.component.html'));
 });
 ngApp.listen(process.env.PORT || 4200);
