08a0ce69fd53b63ab791715dfd517d7d5ee49637
santycano <santy.cano@hotmail.com>
2021-11-06 15:19:11 -0500

    Heroku 7

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
 const app = express();
-app.use(requireHTTPS);
+const path = require('path');
 
-app.use(express.static('./dist/curva-adolescente'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/curva-adolescente/'}
-);
-});
-
+app.use(express.static(__dirname + '/dist/mdb-angular-free'));
 app.listen(process.env.PORT || 8080);
 
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/mdb-angular-free/index.html');
+  console.log(" Fetching from.." + fullPath);
+  res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
