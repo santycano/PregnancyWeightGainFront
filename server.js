16fa78100d9d13729de15f3d952a42ea83865565
santycano <santy.cano@hotmail.com>
2021-11-05 23:22:06 -0500

    Heroku 6

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
-const path = require('path');
-const ngApp = express();
-ngApp.use(express.static('./dist/PregnancyWeightGain'));
-ngApp.get('/*', function (request, response) {
-  response.sendFile(path.join(__dirname, '/src/app/app.component.html'));
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/curva-adolescente'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/curva-adolescente/'}
+);
 });
-ngApp.listen(process.env.PORT || 4200);
+
+app.listen(process.env.PORT || 8080);
+
